36ea7fe30420ee57f6b052f3f35cf3d3c0c0a39d
Cedric <c.tranvanloc@gmail.com>
2024-10-14 20:57:35 +0200

    first try

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const queryString = window.location.search;
 console.log(queryString);
 const newUser = urlParams.get('PID')
 console.log(newUser);
+
+$(document).ready(function() {
+    // Just replacing the value of the 'content' attribute will not work.
+    $('head').append( '<meta name="description" content="'+urlParams.get('PID')+'">' );
+});
