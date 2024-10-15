0877e1db82442dad2eba440924db51dee0136258
Cedric <c.tranvanloc@gmail.com>
2024-10-15 09:55:58 +0200

    testing ico

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ $(document).ready(function() {
     $('head').append( '<meta property="og:title" content="'+title+'" />' );
     $('head').append( '<meta property="og:description" content="'+description+'" />' );
     $('head').append( '<meta property="og:image" content="./img/'+urlParams.get('image')+'" />' );
+    $('head').append( '<link rel="icon" type="image/png" href="./img/'+urlParams.get('image')+'" />' );
     $('body').append( '<img src="./img/'+urlParams.get('image')+'" />' );
 });
