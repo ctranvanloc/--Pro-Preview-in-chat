a246f5bdbceb3971767d593b3eed4f063a52c11f
Cedric <c.tranvanloc@gmail.com>
2024-10-15 09:08:06 +0200

    adding path to img

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const urlParams = new URLSearchParams(window.location.search);
 $(document).ready(function() {
     $('head').append( '<meta property="og:title" content="'+urlParams.get('title')+'">' );
     $('head').append( '<meta property="og:description" content="'+urlParams.get('description')+'">' );
-    $('head').append( '<meta property="og:image" content="'+urlParams.get('image')+'">' );
+    $('head').append( '<meta property="og:image" content="/img/'+urlParams.get('image')+'">' );
 });
