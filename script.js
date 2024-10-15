cd7b44a606e9960c5aaab451d495712a6e5c80cb
Cedric <c.tranvanloc@gmail.com>
2024-10-15 09:52:38 +0200

    fix path?

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const description = urlParams.get('description').replace(/%/g, " ");
 $(document).ready(function() {
     $('head').append( '<meta property="og:title" content="'+title+'" />' );
     $('head').append( '<meta property="og:description" content="'+description+'" />' );
-    $('head').append( '<meta property="og:image" content="img/'+urlParams.get('image')+'" />' );
-    $('body').append( '<img src="img/'+urlParams.get('image')+'" />' );
+    $('head').append( '<meta property="og:image" content="./img/'+urlParams.get('image')+'" />' );
+    $('body').append( '<img src="./img/'+urlParams.get('image')+'" />' );
 });
